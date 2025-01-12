7a174e6cc12e9dc5c65731188687647059c01de3
phucdang2002 <131520774+phucdang2002@users.noreply.github.com>
2025-01-12 15:22:33 +0700

    add ios and test server

--- ServerWebsocket.js
+++ ServerWebsocket.js
@@ -1,21 +1,41 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8084 })
+const server = new WebSocket.Server({ port: 8084 });
+
 server.on('connection', ws => {
     console.log('Client connected');
+
     ws.on('message', message => {
-        // console.log(`Received: ${message}`);
         const data = JSON.parse(message);
-        console.log(data);
-        
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(`Broadcast: ${message}`);
+                let sendData = {};
+                const uniqueId = Date.now() + Math.random(); // Generate a unique ID
+
+                if (data.type === 'connect') {
+                    sendData = {
+                        id: uniqueId,
+                        type: 'connect',
+                        name: data.name,
+                        message: `${data.name} has connected`
+                    };
+                } else if (data.type === 'message') {
+                    sendData = {
+                        id: uniqueId,
+                        type: 'message',
+                        name: data.name,
+                        message: data.message
+                    };
+                } else if (data.type === 'broadcast') {
+                    sendData = {
+                        id: uniqueId,
+                        type: 'broadcast',
+                        message: `Broadcast: ${message}`
+                    };
+                }
+                client.send(JSON.stringify(sendData));
             }
-        })
-    })
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    })
-})
+        });
+    });
+});
 console.log("Server is running on port 8084");
 
--- src/GroupScreen.js
+++ src/GroupScreen.js
@@ -1,29 +1,52 @@
-import { useEffect } from "react"
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-const GroupScreen = ({route}) => {
-    const {name} = route.params
-    const ws = new WebSocket('ws://10.60.246.50:8084')
+import { useEffect, useState } from "react"
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
+import Icon from 'react-native-vector-icons/MaterialIcons'
+const GroupScreen = ({ route }) => {
+    const { name } = route.params
+    const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState('')
+    const ws = new WebSocket('ws://192.168.1.3:8084')
     useEffect(() => {
         ws.onopen = () => {
-            ws.send(JSON.stringify({type: 'connect', name: name}))
+            ws.send(JSON.stringify({ type: 'connect', name: name }))
+        }
+        ws.onmessage = (e) => {
+            const data = JSON.parse(e.data)
+            console.log(data);
+            setMessages([...messages, data])
+            console.log(messages);
         }
         return () => {
             ws.close();
         };
+
     }, [])
+    const sendMessage = () => {
+        ws.send(JSON.stringify({ type: 'message', name: name, message: message }))
+        setMessage('')
+    }
     return (
-        <View style={{flex: 1}}>
-            <Text>Group Chat</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+                data={messages}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => {
+                    item.type === 'connect' ? <Text>{item.message}</Text> : (
+                        <View style={name === item.name ? styles.sender : styles.receiver}>
+                            <Text>{item.name}: {item.message}</Text>
+                        </View>
+                    )
+                }}
+            />
             <View style={styles.messageBox}>
-                <Icon name="paperclip" size={20} color="#000"/>
-                <TextInput style={styles.textBox} placeholder="Type a message"/>
-                <Icon name="content-copy" size={20} color="#000"/>
-                <TouchableOpacity style={styles.sendBtn} onPress={()=>{}}>
-                    <Icon name="send" size={20} color="#FFF"/>
+                <Icon name="attach-file" size={20} color="#000" />
+                <TextInput style={styles.textBox} value={message} onChangeText={setMessage} placeholder="Type a message" />
+                <Icon name="content-copy" size={20} color="#000" />
+                <TouchableOpacity style={styles.sendBtn} onPress={() => sendMessage()}>
+                    <Icon name="send" size={20} color="#FFF" />
                 </TouchableOpacity>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
@@ -37,6 +60,22 @@ const styles = StyleSheet.create({
         padding: 15,
         borderTopWidth: 3,
         borderTopColor: '#ddd',
+    },
+    sender: {
+        width: '100%',
+        backgroundColor: '#24786D',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'flex-end',
+        marginBottom: 10,
+    },
+    receiver: {
+        width: '100%',
+        backgroundColor: '#ddd',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'flex-start',
+        marginBottom: 10,
     },
     textBox: {
         width: '70%',
